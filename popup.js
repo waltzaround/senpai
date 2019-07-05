0d51c3fc501893fb79df66cc4af367307f70b5b5
Walter Lim <waltissomewhere@gmail.com>
2019-07-05 22:55:25 +1200

    styling

--- popup.js
+++ popup.js
@@ -1,23 +1,54 @@
-$(function() {
-  //default value is "start"
-  var currentState = localStorage.currentState || "start";
-  //cache button DOM element reference
-  var $toggleBtn = $("#toggle-btn");
+var actionstate = false;
+function trueaction() {
+console.log("become inactive")
+document.getElementById("senpaiState").style.color = '#303030';
+}
 
-  //update button status
-  if (currentState === "stop") {
-    $toggleBtn.text("OFF");
+function falseaction(){
+  console.log("become active")
+}
+
+
+
+console.log("initial state is " + actionstate);
+document.getElementsByClassName('switch')[0].addEventListener("click", (e) => {
+  e.preventDefault();
+  
+  if (actionstate === true) {
+    trueaction();
+
+  }
+  if (actionstate === false) {
+    falseaction();
+
   }
 
-  //register button click handler
-  $toggleBtn.click(function() {
-    if (currentState === "start") {
-      $toggleBtn.text("OFF");
-      localStorage.currentState = "stop";
-    }
-    if (currentState === "stop") {
-      $toggleBtn.text("ON");
-      localStorage.currentState = "start";
-    }
-  });
+  actionstate = !actionstate;
+  console.log("current state is " + actionstate);
+  document.getElementById("check1").checked = actionstate;
 });
+
+
+// $(function() {
+//   //default value is "start"
+//   var currentState = localStorage.currentState || "start";
+//   //cache button DOM element reference
+//   var $toggleBtn = $("#toggle-btn");
+
+//   //update button status
+//   if (currentState === "stop") {
+//     $toggleBtn.text("OFF");
+//   }
+
+//   //register button click handler
+//   $toggleBtn.click(function() {
+//     if (currentState === "start") {
+//       $toggleBtn.text("OFF");
+//       localStorage.currentState = "stop";
+//     }
+//     if (currentState === "stop") {
+//       $toggleBtn.text("ON");
+//       localStorage.currentState = "start";
+//     }
+//   });
+// });
